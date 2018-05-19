86608f3f28c3194d0f873aa87804ea475313fb28
Suparn B <rockycool00007@gmail.com>
2018-05-19 00:43:17 -0700

    Added google maps and content layout

--- js/home.js
+++ js/home.js
@@ -29,10 +29,34 @@ $(document).ready(function() {
                             }, 2000);
                         });
 
-                        $("#lnk_whoarewe_scroll").click(function(){
-                            alert("test");
+                        $("#whoarewe-scroll").click(function(){
                             $('html, body').animate({
                                 scrollTop: $("#whoarewe-container").offset().top
                             }, 2000);
                         });
+
+                        $("#projectgallery-scroll").click(function(){
+                            $('html, body').animate({
+                                scrollTop: $("#projectgallery-container").offset().top
+                            }, 2000);
+                        });
+
+
+
+                        function initialize() {
+                            var myLatlng = new google.maps.LatLng(49.0643980, -122.3334560);
+                            var mapOptions = {
+                              zoom: 15,
+                              center: myLatlng,
+                              mapTypeId: google.maps.MapTypeId.ROADMAP
+                            }
+                                var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+                            var marker = new google.maps.Marker({
+                              position: myLatlng,
+                              map: map,
+                              title: 'Contact'
+                            }
+                                                               );
+                          }
+                          google.maps.event.addDomListener(window, 'load', initialize);
 });
